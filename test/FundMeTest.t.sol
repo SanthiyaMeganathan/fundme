b18a4fbb9ba1551a55aad243db0018ac794bb3fd
SanthiyaMeganathan <meganathankmsanthiya@gmail.com>
2024-10-07 17:29:38 +0530

    fundme

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,11 +1,27 @@
-//SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/Fundme.sol";
 
 contract FundMeTest is Test {
-    function setUp() external {}
+    FundMe fundme;
 
-    function testdemo() public {}
+    function setUp() external {
+        fundme = new FundMe();
+    }
+
+    function testMinimumDollarIsFiver() public view {
+        // Call the function MINIMUM_USD() to get its value
+        assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        console.log(msg.sender);
+        console.log(fundme.i_owner());
+
+        console.log(address(this));
+        assertEq(fundme.i_owner(), address(this));
+    }
 }
