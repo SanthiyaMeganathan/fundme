d9c978985864c7a123b8ec8c872f9e1bd894c429
SanthiyaMeganathan <meganathankmsanthiya@gmail.com>
2024-10-18 16:55:55 +0530

    fundme

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.27;
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {console} from "forge-std/console.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract HelperConfig is Script {
     uint8 public DECIMAL;
@@ -50,11 +52,21 @@ contract HelperConfig is Script {
         //price feed address
     }
 
-    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+    function getMainnetEthConfig() public view returns (NetworkConfig memory) {
         //price feed address
         NetworkConfig memory mainnetConfig = NetworkConfig({
             priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
         });
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            mainnetConfig.priceFeed
+        );
+
+        // Get the version of the price feed
+        uint256 version = priceFeed.version();
+
+        // Log the version to the console
+        console.log("Price Feed Version:", version);
+
         return mainnetConfig;
     }
 }
